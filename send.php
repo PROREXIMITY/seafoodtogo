929c054a1556b5f44f55e8e209a28ecd6cfbb4a4
xyroast <darcenxc@students.nu-baliwag.edu.ph>
2023-04-15 08:37:59 +0800

    Added PHPmailer

---
+++ send.php
@@ -0,0 +1,33 @@
+<?php 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+if(isset($_POST["send"])){
+  $mail = new PHPMailer(true);
+
+  $mail->isSMTP();
+  $mail->Host = 'smtp.gmail.com';
+  $mail->SMTPAuth = true;
+  $mail->Username='xhdarcen@gmail.com';
+  $mail->Password='mmor qcrp scbd jddx';
+  $mail->SMTPSecure='ssl';
+  $mail->Port= 465;
+
+  $mail->setFrom('xhdarcen@gmail.com');
+
+  $mail->addAddress($_POST["email"]);
+
+  $mail->isHTML(true);
+
+  $mail->Subject= $_POST["subject"];
+  $mail->Body = $_POST["message"];
+
+  $mail->send();
+
+  echo "<script> alert('Sent Successfully'); document.location.href = 'index.html';";
+}
+?>
