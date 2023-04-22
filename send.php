5c7a7959610da4a75ca7361ae4df4a0285c7b9f9
xyroast <darcenxc@students.nu-baliwag.edu.ph>
2023-04-23 06:58:36 +0800

    Added phpmailer

--- send.php
+++ send.php
@@ -1,16 +1,22 @@
 
 <?php
+$email = $_POST["email"];
+$name = $_POST["name"];
+$subject = $_POST["subject"];
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+// use PHPMailer\PHPMailer\SMTP;
  
 require 'phpmailer/src/Exception.php';
 require 'phpmailer/src/PHPMailer.php';
 require 'phpmailer/src/SMTP.php';
+
  
-if(isset($_POST["send"])){
-    $email = $_POST["email"];
-    $name = $_POST["name"];
-    $subject = $_POST["subject"];
+$mail = new PHPMailer(true);
+try {
+    // $email = $_POST["email"];
+    // $name = $_POST["name"];
+    // $subject = $_POST["subject"];
  
     $msg ='<!DOCTYPE html>
     <html lang="en"> 
@@ -22,31 +28,36 @@ if(isset($_POST["send"])){
         <p>Thank you for your feedback, this is truly noted, Thanks '.$name.'!</p>
         <br><br>
         Have a great day ahead!,<br>
-        RomeCita Staff<br>
+        Seafood to Go Staff<br>
         <strong></b> SeafoodtoGo</strong>
  
     </body>
     </html>';
  
  
-    $mail = new PHPMailer(true);
+    // $mail = new PHPMailer(true);
     $mail->isSMTP();
-    $mail->Host = "smtp.gmail.com";
-    $mail->SMTPAuth = "true";
-    $mail->Username = "xhdarcen@gmail.com";
-    $mail->Password = "imavxuteciqavcro";
-    $mail->SMTPSecure = "tls";
-    $mail->Port = "587";
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'xhdarcen@gmail.com';
+    $mail->Password = 'oxmzwryklbsbemne';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+    $mail->SMTPOptions = array('ssl' => array('verify_peer'       => false,
+                                          'verify_peer_name'  => false,
+                                          'allow_self_signed' => true));
  
-    $mail->setFrom("xhdarcen@gmail.com", "Seafood to Go Management");
+    $mail->setFrom('xhdarcen@gmail.com', 'Seafood to Go Staff');
  
     $mail->addAddress($email); 
+    
     $mail->isHTML(true);
- 
     $mail->Subject = $subject;
     $mail->Body = $msg;
  
     $mail->send();
-    header('location:index.html');
+    echo 'Sent!';
+} catch (Exception $e){
+    echo 'Error Sending!';
 }
-?>
+    header('location: index.php');
